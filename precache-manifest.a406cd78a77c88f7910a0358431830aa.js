39c02213799f9884fad383d4d14af097b5f8261a
Luke Liu <lukeliu@berkeley.edu>
2020-05-24 11:12:56 -0400

    Updates

---
+++ precache-manifest.a406cd78a77c88f7910a0358431830aa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b14c7d1ba65bc7c65bfe9c2e637f927a",
+    "url": "/ravenous/index.html"
+  },
+  {
+    "revision": "6f1435ac694f63081264",
+    "url": "/ravenous/static/css/main.8b316694.chunk.css"
+  },
+  {
+    "revision": "46fdade03e7e2c55dfca",
+    "url": "/ravenous/static/js/2.0191b2a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ravenous/static/js/2.0191b2a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f1435ac694f63081264",
+    "url": "/ravenous/static/js/main.2f0443d8.chunk.js"
+  },
+  {
+    "revision": "77cc78b45e8520379e14",
+    "url": "/ravenous/static/js/runtime-main.80268069.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
